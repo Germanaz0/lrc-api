6a2b135519d9355f005438343746795f6f65684d
German Bortoli <bortolig@dnb.com>
2019-09-13 15:11:24 -0300

    Improvements of dummy data

--- database/seeds/DummieDataSeeder.php
+++ database/seeds/DummieDataSeeder.php
@@ -11,7 +11,21 @@ class DummieDataSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\User::class, 50)->create();
+        // Create services
         factory(App\Models\Service::class, 5000)->create();
+
+        // Front user
+        $data = [
+            'name' => 'Tester user',
+            'email' => 'tester@lrc.example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+
+        App\Models\User::create($data);
+
+        // Let's create 5 more users
+        factory(App\Models\User::class, 5)->create();
     }
 }

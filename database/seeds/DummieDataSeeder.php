45714f1b30f0ccd316397909ee9853392a2d3492
German Bortoli <bortolig@dnb.com>
2019-09-13 10:52:36 -0300

    Added users seeder

--- database/seeds/DummieDataSeeder.php
+++ database/seeds/DummieDataSeeder.php
@@ -11,7 +11,7 @@ class DummieDataSeeder extends Seeder
      */
     public function run()
     {
-//        factory(App\Models\User::class, 50)->create();
+        factory(App\Models\User::class, 50)->create();
         factory(App\Models\Service::class, 5000)->create();
     }
 }

cf5a152050bcf6a715d7d2714a042475a09e6239
German Bortoli <germanazo@gmail.com>
2019-09-13 09:19:19 -0300

    Feature/passport authentication (#1)
    * Added authentication
    
    * Added postman docs
    
    * - Added user authentication methods.
    - Added unit tests.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use Illuminate\Http\Request;
+use App\User;
+use Carbon\Carbon;
+use Auth;
+
+/**
+ * Class AuthController
+ * @package App\Http\Controllers
+ */
+class AuthController extends Controller
+{
+    /**
+     * Register a user to the system
+     * @param UserRegisterRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function signup(UserRegisterRequest $request)
+    {
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json(['message' => 'Successfully created user!'], 201);
+    }
+
+    /**
+     * Login user using passport
+     * @param UserLoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        // Validate credentials through user
+        if (!Auth::guard('web')->attempt($credentials, false, false)) {
+            return response()->json(['message' => 'Wrong password or email'], 401);
+        }
+
+        // Get current user
+        $user = User::where(['email' => $credentials['email']])->first();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+
+        // Extend token lifetime
+        if ($request->remember_me) {
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        }
+
+        $token_expires = Carbon::parse($tokenResult->token->expires_at)->toDateTimeString();
+
+        // Save user
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => $token_expires,
+        ]);
+    }
+
+    /**
+     * Logout User, revoke access token
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Get logged in user information
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me(Request $request)
+    {
+        return response()->json($request->user());
+
+    }
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use \App\User;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions, WithFaker;
+
+    const URL_SIGNUP = 'api/auth/signup';
+    const URL_LOGIN = 'api/auth/login';
+    const URL_LOGOUT = 'api/auth/logout';
+    const URL_ME = 'api/auth/me';
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testSignup()
+    {
+        // Generate an initial form with a wrong password (lenght)
+        $form = [
+            'name' => 'Tester',
+            'email' => 'tester@tester.com',
+            'password' => '1',
+            'password_confirmation' => '1',
+        ];
+
+        // Test invalid length
+        $response = $this->json('POST', self::URL_SIGNUP, $form);
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors' => [
+                    'password' => [],
+                ],
+            ]);
+
+        // Test unmatching passwords
+        $form['password'] = 'password';
+        $form['password_confirmation'] = 'password-does-not-match';
+
+        $response = $this->json('POST', self::URL_SIGNUP, $form);
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors' => [
+                    'password' => [],
+                ],
+            ]);
+
+        // Test empty name
+        $form['password'] = 'password';
+        $form['password_confirmation'] = 'password';
+        $form['name'] = '';
+
+        $response = $this->json('POST', self::URL_SIGNUP, $form);
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors' => [
+                    'name' => [],
+                ],
+            ]);
+
+        // Finally register user
+        $form['password'] = 'password';
+        $form['password_confirmation'] = 'password';
+        $form['name'] = 'Super Tester';
+
+        $response = $this->json('POST', self::URL_SIGNUP, $form);
+        $response
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'message',
+            ]);
+
+        // Test unique emails it should fail
+        $response = $this->json('POST', self::URL_SIGNUP, $form);
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'errors' => [
+                    'email' => [],
+                ],
+            ]);
+    }
+
+    /**
+     * Test login feature
+     */
+    public function testLogin()
+    {
+        // Creates user to login
+        $user = factory(User::class)->create();
+
+        $form = [
+            'email' => $user->email,
+            'password' => 'wrong-password', // Good: password
+            'remember_me' => false,
+        ];
+
+        $response = $this->json('POST', self::URL_LOGIN, $form);
+        $response
+            ->assertStatus(401)
+            ->assertJsonStructure([
+                'message',
+            ]);
+
+        // Finally login
+        $form['password'] = 'password';
+        $response = $this->json('POST', self::URL_LOGIN, $form);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token',
+                'token_type',
+                'expires_at',
+            ]);
+
+        // Test valid loogged in  user
+        $access_token = $response->json(['access_token']);
+        $headers = [
+            'Authorization' => "Bearer ${access_token}",
+        ];
+
+        $response = $this->json('GET', self::URL_ME, [], $headers);
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'id',
+            'name',
+            'email',
+            'created_at',
+            'updated_at',
+        ]);
+
+        // Test Logout
+        $access_token = $response->json(['access_token']);
+        $headers = [
+            'Authorization' => "Bearer ${access_token}",
+        ];
+
+        $response = $this->json('GET', self::URL_LOGOUT, [], $headers);
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'message',
+            ]);
+    }
+}
